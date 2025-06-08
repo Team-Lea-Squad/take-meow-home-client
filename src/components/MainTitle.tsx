8f9ada6f11cbb8782f08d9e3890ebde6067da472
FE LEA <88094675+leaisrevolution@users.noreply.github.com>
2025-06-08 18:06:33 +0900

    Add: 헤더 컴포넌트, 메인 타이틀 컴포넌트

---
+++ src/components/MainTitle.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const MainTitle = ({ mainTitle }: { mainTitle: string }) => {
+  return (
+    <div className="whitespace-pre-line flex items-center text-head-sub text-text-black py-1000 px-600">
+      {mainTitle}
+    </div>
+  );
+};
+
+export default MainTitle;
---
+++ src/utils/titleAssets.ts
@@ -0,0 +1,4 @@
+export const mainTitles = {
+  shelterRegistration: "입양공고 등록을 위한 \n 보호소 정보를 등록해주세요",
+  selectAddress: "주소를 선택해주세요"
+};

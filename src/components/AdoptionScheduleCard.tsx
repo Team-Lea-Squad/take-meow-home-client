c8fa25633a22584409447e7d5dc8911682cddd78
FE LEA <88094675+leaisrevolution@users.noreply.github.com>
2025-06-10 04:41:39 +0900

    Add: 보호소 메인 화면

---
+++ src/components/AdoptionScheduleCard.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+
+const AdoptionScheduleCard = () => {
+  const router = useRouter();
+
+  return (
+    <div className="rounded-lg p-500 bg-background-chips gap-2 flex flex-col"></div>
+  );
+};
+
+export default AdoptionScheduleCard;
---
+++ src/components/RegisterCatCard.tsx
@@ -0,0 +1,34 @@
+"use client";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+
+const RegisterCatCard = () => {
+  const router = useRouter();
+  const [hasRegisteredCats, setHasRegisteredCats] = useState(false);
+
+  return (
+    <div className="rounded-lg p-500 bg-background-chips gap-2 flex flex-col">
+      {!hasRegisteredCats && (
+        <p
+          className="whitespace-pre text-text-black text-paragraph-120-R"
+          onClick={() => {
+            router.push("");
+          }}
+        >{`등록된 고양이가 없습니다. \n고양이를 등록해주세요.`}</p>
+      )}
+
+      <div className="text-text-black text-paragraph-150-B flex items-center">
+        고양이 등록하기
+        <Image
+          src="/icons/arrow-right.svg"
+          alt="right-button"
+          width={18}
+          height={18}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default RegisterCatCard;

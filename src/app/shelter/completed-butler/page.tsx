7f8fe3e441913d4b8f0c16cab03f56d1a19b6fbe
FE LEA <88094675+leaisrevolution@users.noreply.github.com>
2025-06-10 17:03:50 +0900

    Edit: 바텀시트 애니메이션 추가

--- src/app/shelter/completed-butler/page.tsx
+++ src/app/shelter/completed-butler/page.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import MainTitle from "@/components/MainTitle";
 import { mainTitles } from "@/utils/titleAssets";
+import CommonButton from "@/components/CommonButton";
 
 const Page = () => {
   return (
-    <div>
+    <div className="flex flex-col">
       <MainTitle mainTitle={mainTitles.butlerSelectionComplete} />
+      <div className="flex flex-col gap-2 py-4">
+        <CommonButton title="보호소 메인으로 가기" />
+        <CommonButton title="보호소 메인으로 가기" />
+      </div>
     </div>
   );
 };

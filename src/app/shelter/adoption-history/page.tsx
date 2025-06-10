944b0bc0e8314aa0e1574a7eea9631b20a08223d
FE LEA <88094675+leaisrevolution@users.noreply.github.com>
2025-06-10 17:35:47 +0900

    Edit: 분양 완료 버튼 및 현재 시간 추가

--- src/app/shelter/adoption-history/page.tsx
+++ src/app/shelter/adoption-history/page.tsx
@@ -1,9 +1,33 @@
+"use client";
+
 import React from "react";
 import MainTitle from "@/components/MainTitle";
 import { mainTitles } from "@/utils/titleAssets";
 import BottomButton from "@/components/BottomButton";
+import { useAdoptionStore } from "@/store/adoptionStore";
 
 const Page = () => {
+  const isAdopted = useAdoptionStore((state) => state.isAdopted);
+  const setIsAdopted = useAdoptionStore((state) => state.setIsAdopted);
+  const adoptedTime = useAdoptionStore((state) => state.adoptedTime);
+  const setAdoptedTime = useAdoptionStore((state) => state.setAdoptedTime);
+
+  const handleAdopt = () => {
+    setIsAdopted(true);
+    const now = new Date();
+    const hh = now.getHours().toString().padStart(2, "0");
+    const mm = now.getMinutes().toString().padStart(2, "0");
+    setAdoptedTime(`${hh}:${mm}`);
+  };
+
+  const today = new Date();
+  const days = ["일", "월", "화", "수", "목", "금", "토"];
+  const formattedDate = `${today.getFullYear()}/${(today.getMonth() + 1)
+    .toString()
+    .padStart(2, "0")}/${today.getDate().toString().padStart(2, "0")} ${
+    days[today.getDay()]
+  }`;
+
   return (
     <div className="flex flex-col gap-4">
       <MainTitle mainTitle={mainTitles.adoptionHistory} />
@@ -22,11 +46,19 @@ const Page = () => {
         <p className="text-paragraph-150-B">2025/06/10 화</p>
         <p>김혜원님의 방문일을 수락하였습니다.</p>
       </div>
-      <div className="flex flex-col gap-2">
-        <p className="text-paragraph-150-B">2025/06/10 화</p>
-        <p>김혜원님의 분양이 완료되었습니다.</p>
-      </div>
-      <BottomButton buttonTitle="분양 완료" />
+      {isAdopted && (
+        <div className="flex flex-col gap-2">
+          <p className="text-paragraph-150-B">
+            {formattedDate} {adoptedTime && <span>{adoptedTime}</span>}
+          </p>
+          <p>🎉 김혜원님의 분양이 완료되었습니다.</p>
+        </div>
+      )}
+      <BottomButton
+        buttonTitle="분양 완료"
+        disabled={isAdopted}
+        onClick={handleAdopt}
+      />
     </div>
   );
 };

017727bbf79c52d7b4f1fd40d53ea07813e2ec97
FE LEA <88094675+leaisrevolution@users.noreply.github.com>
2025-06-14 01:01:27 +0900

    Edit: 고양이 정보 등록 컴포넌트

---
+++ src/app/shelter/register-cat/_components/CatBasicInfo.tsx
@@ -0,0 +1,57 @@
+"use client";
+import React, { useState } from "react";
+import { registerCatAssets } from "@/utils/registerCatAssets";
+import MainTitle from "@/components/MainTitle";
+import InputForm from "@/components/InputForm";
+import ToggleButton from "./ToggleButton";
+
+const CatBasicInfo = () => {
+  const [inputs, setInputs] = useState({
+    catName: "",
+    catAge: "",
+    catGender: ""
+  });
+
+  return (
+    <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-2">
+        <MainTitle mainTitle={registerCatAssets.name.title} />
+        <InputForm
+          value={inputs.catName}
+          onChange={(e) => setInputs({ ...inputs, catName: e.target.value })}
+        />
+      </div>
+      <div className="flex flex-col gap-4 w-full">
+        <MainTitle mainTitle={registerCatAssets.physical.title} />
+        <div className="w-full flex gap-4 justify-center items-center">
+          <div className="p-2 w-1/3 items-center justify-center">성별</div>
+          <ToggleButton />
+        </div>
+        <div className="w-full flex gap-4 justify-center items-center">
+          <div className="p-2 w-1/3 items-center justify-center">나이</div>
+          <InputForm
+            rightText="살"
+            value={inputs.catGender}
+            isNumber
+            onChange={(e) =>
+              setInputs({ ...inputs, catGender: e.target.value })
+            }
+          />
+        </div>
+        <div className="w-full flex gap-4 justify-center items-center">
+          <div className="p-2 w-1/3 items-center justify-center">몸무게</div>
+          <InputForm
+            rightText="kg"
+            value={inputs.catGender}
+            isNumber
+            onChange={(e) =>
+              setInputs({ ...inputs, catGender: e.target.value })
+            }
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CatBasicInfo;
--- src/utils/registerCatAssets.ts
+++ src/utils/registerCatAssets.ts
@@ -1,11 +1,9 @@
-export const registerCatAssets = [
-  {
-    기본정보: [
-      { title: "고양이의 이름/별명은 무엇인가요?", isOptional: false },
-      { title: "고양이의 성별을 알려주세요.", isOptional: false },
-      { title: "고양이의 나이는 어떻게 되나요?", isOptional: false },
-      { title: "고양이의 신체 정보를 입력해주세요.", isOptional: false }
-    ],
-    건강: ["예방접종", "중성화 여부", "질병", "특이사항", "건강검진"]
+export const registerCatAssets = {
+  name: { title: "고양이의 이름/별명은 무엇인가요?", isOptional: false },
+  physical: { title: "고양이의 신체 정보를 입력해주세요.", isOptional: false },
+  health: { title: "고양이의 건강 정보를 선택해주세요.", isOptional: false },
+  personality: {
+    title: "고양이의 성격 정보를 선택해주세요.",
+    isOptional: false
   }
-];
+};

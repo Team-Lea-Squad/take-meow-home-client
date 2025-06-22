aec907d7da0bc90c1f6d1e23129b7f5a62906914
FE LEA <88094675+leaisrevolution@users.noreply.github.com>
2025-06-22 23:28:35 +0900

    Add: 메인 헤더 로고 및 알림 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,17 +9,18 @@ const Header = () => {
   const pathname = usePathname();
   const router = useRouter();
   const isLogin = pathname === "/login";
+  const isHome = pathname === "/";
 
   const headerTitle = (() => {
-    if (isLogin) return "";
-    if (pathname === "/") return "";
+    if (isLogin || isHome) return "";
+    // if (pathname.startsWith("/onboarding")) return "추가 정보 입력";
     return "";
   })();
 
   return (
     <div className="w-full h-16 bg-background-white text-text-dark p-600 flex justify-between items-center fixed top-0 z-3">
       <div className="flex gap-400 text-body-120-M items-center">
-        {!isLogin && (
+        {!isLogin && !isHome && (
           <Image
             src="/icons/header-left.svg"
             alt="left-button"
@@ -29,9 +30,18 @@ const Header = () => {
             onClick={() => router.back()}
           />
         )}
-        <p>{headerTitle}</p>
+        {isHome ? (
+          <Image
+            src="/take-meow-home-logo.svg"
+            alt="take meow home logo"
+            width={69}
+            height={18}
+          />
+        ) : (
+          <p>{headerTitle}</p>
+        )}
       </div>
-      {!isLogin && (
+      {!isLogin && !isHome && (
         <Image
           src="/icons/close.svg"
           alt="close-icon"
@@ -40,6 +50,12 @@ const Header = () => {
           style={{ cursor: "pointer" }}
           onClick={() => router.push("/")}
         />
+      )}
+      {isHome && (
+        <div className="flex gap-1 h-5">
+          알림
+          <div className="rounded-full bg-red-600 w-[6px] h-[6px]"></div>
+        </div>
       )}
     </div>
   );

f423edb6bba81c5a535ed5873bc3b3a46594ff14
FE LEA <88094675+leaisrevolution@users.noreply.github.com>
2025-06-23 00:38:06 +0900

    Add: 퀴즈 컴포넌트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@
 import MainTitle from "@/components/MainTitle";
 import { mainTitles } from "@/utils/titleAssets";
 import AddButler from "@/components/AddButler";
+import Quiz from "@/components/Quiz";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-4">
       <MainTitle mainTitle={mainTitles.homeTitle} userName="냥냥냥" />
       <AddButler />
+      <Quiz />
       {/* <SubTab />
       <CommonButton title="테스트버튼" isDisabled />
       <CommonButton title="테스트버튼" isPoint />
